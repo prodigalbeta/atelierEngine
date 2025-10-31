78673daa93da59f00b2239f92ae98490bbe63a2f
prodigalbeta <137588668+prodigalbeta@users.noreply.github.com>
2025-10-31 12:52:38 -0700

    create window test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,52 @@
 #include <iostream>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main () {
-  SDL_Init(SDL_INIT_VIDEO);
+void cleanup(SDL_Window *win);
 
+int main (int argc, char *argv[]) {
+  if (!SDL_Init(SDL_INIT_VIDEO))
+  {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error initializing SDL3", nullptr);
+    return 1;
+  }
+
+  // creating the window
+  int width = 800;
+  int height = 600;
+  SDL_Window *win = SDL_CreateWindow("SDL3 Demo", width, height, 0);
+  if (!win)
+  {
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error", "Error creating window", win);
+    cleanup(win);
+    return 1;
+  }
   std::cout << "Hello World!\n";
 
-  SDL_Quit();
+  //starting the game loop
+  bool running = true;
+  while (running)
+  {
+    SDL_Event event{ 0 };
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_EVENT_QUIT:
+        {
+          running = false;
+          break;
+        }
+      }
+    }
+  }
+
+  cleanup(win);
   return 0;
 }
 
+void cleanup(SDL_Window *win)
+{
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+}
